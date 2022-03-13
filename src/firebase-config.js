411a6b7d407ec5d6c7f48ebf3e58459e919f7100
Nilshree123 <91187870+Nilshree123@users.noreply.github.com>
2022-03-13 22:26:44 +0530

    Testing

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,16 +1,16 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "@firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBG_9s2JJDUeBLtMenyPtIBsVLupa8vRB8",
-  authDomain: "fir-tutorial-ad573.firebaseapp.com",
-  projectId: "fir-tutorial-ad573",
-  storageBucket: "fir-tutorial-ad573.appspot.com",
-  messagingSenderId: "459866772432",
-  appId: "1:459866772432:web:b029523d67a8c9f1981fd5",
-  measurementId: "G-2R62T7YE0E",
-};
 
-const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+// import firebase from 'firebase/compat/app'
+// import 'firebase/firestore'
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBGLCVCVS2d0vqpCyNxHYFi0QROxDr0t5w",
+//   authDomain: "authoropod-images.firebaseapp.com",
+//   projectId: "authoropod-images",
+//   storageBucket: "authoropod-images.appspot.com",
+//   messagingSenderId: "691614240479",
+//   appId: "1:691614240479:web:dcc933b5264969a119a36a"
+//   };
+// firebase.initializeApp(firebaseConfig);
+// var storage = firebase.storage();
+// export default storage;
